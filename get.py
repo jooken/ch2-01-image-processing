a845e2d62460a670de061d2486be49c41d4cf997
jooken <jooken@naver.com>
2021-03-09 12:05:23 +0900

    use pillow

--- get.py
+++ get.py
@@ -2,3 +2,9 @@ import requests
 
 url = 'https://bit.ly/3ccGLhN'
 r = requests.get(url, stream=True).raw
+
+from PIL import Image
+
+img = Image.open(r)
+img.save('src.png')
+print(img.get_format_mimetype)
