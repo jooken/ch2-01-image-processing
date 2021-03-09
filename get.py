ce73d7af2cec3f993ff4d89462d2be96ba803eca
jooken <jooken@naver.com>
2021-03-09 12:10:21 +0900

    file read and write

--- get.py
+++ get.py
@@ -8,3 +8,11 @@ from PIL import Image
 img = Image.open(r)
 img.save('src.png')
 print(img.get_format_mimetype)
+
+BUF_SIZE=1024
+with open('src.png', 'rb') as sf, open('dst.png', 'wb') as df:
+    while True:
+        data = sf.read(BUF_SIZE)
+        if not data:
+            break
+        df.write(data)
